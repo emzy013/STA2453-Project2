8d80d82ab85d08d326dd9b112ccf34e3d5d72f7b
Siyuan Shang <cjbbt12@gmail.com>
2022-03-14 00:37:14 -0400

    Add pie chart

--- server.R
+++ server.R
@@ -23,9 +23,17 @@ full_cases <- read.csv(url(cases_link_1))
 vaccines_link_1 <-
     "https://health-infobase.canada.ca/src/data/covidLive/vaccination-coverage-map.csv"
 vaccines <- read.csv(url(vaccines_link_1))
+colnames(vaccines)[1] <- "date"
+vaccines$prop18plus_atleast1dose[vaccines$prop18plus_atleast1dose == ">=99"] <-
+    "99"
 
-
-
+vaccines_link_2 <-
+    "https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-casesAfterVaccination.csv"
+vaccines2 <- read.csv(url(vaccines_link_2)) %>%
+    select("label.en",
+           "prop_cases",
+           "prop_hospitalizations",
+           "prop_deaths")
 
 
 # Define server logic
@@ -85,7 +93,7 @@ shinyServer(function(input, output) {
         colnames(vaccines)[1] <- "date"
         if (input$vac_dose == "Partially vaccinated") {
             vaccines <- mutate(vaccines, dose = numtotal_partially)
-        } else if(input$vac_dose =="At least 1 dose vaccinated"){
+        } else if (input$vac_dose == "At least 1 dose vaccinated") {
             vaccines <- mutate(vaccines, dose = proptotal_atleast1dose)
         } else if (input$vac_dose == "Fully vaccinated") {
             vaccines <- mutate(vaccines, dose = numtotal_fully)
@@ -118,54 +126,125 @@ shinyServer(function(input, output) {
     })
     
     output$vac_percentage <-  renderHighchart({
-        colnames(vaccines)[1] <- "date"
-        df_vac3 <- vaccines  %>%
+        # colnames(vaccines)[1] <- "date"
+        # vaccines$prop18plus_atleast1dose[vaccines$prop18plus_atleast1dose ==">=99"] <- "99"
+        if (input$age_group == "People") {
+            temp <- vaccines
+        } else if (input$age_group == "People 18 and older") {
+            temp <- vaccines  %>%
+                mutate(
+                    proptotal_atleast1dose = as.numeric(prop18plus_atleast1dose),
+                    proptotal_partially = prop18plus_partially,
+                    proptotal_fully = prop5plus_fully,
+                    proptotal_additional = prop5plus_additional
+                )
+        } else if (input$age_group == "People 5 and older") {
+            temp <- vaccines  %>%
+                mutate(
+                    proptotal_atleast1dose = prop5plus_atleast1dose,
+                    proptotal_partially = prop5plus_partially,
+                    proptotal_fully = prop5plus_fully,
+                    proptotal_additional = prop5plus_additional
+                )
+        }
+        
+        df_vac3 <- temp  %>%
             mutate(date = lubridate::ymd(date)) %>%
-            select(c("date",
-                     "prename",
-                     "proptotal_atleast1dose",
-                     "proptotal_partially",
-                     "proptotal_fully",
-                     "proptotal_additional"))
+            select(
+                c(
+                    "date",
+                    "prename",
+                    "proptotal_atleast1dose",
+                    "proptotal_partially",
+                    "proptotal_fully",
+                    "proptotal_additional"
+                )
+            )
         
         df_vac3 <- df_vac3 %>%
             filter(prename == input$area_percentage)
-
+        
         plot <- df_vac3 %>%
-            hchart(., "line",
+            hchart(.,
+                   "line",
                    hcaes(
                        x = date,
                        y = proptotal_atleast1dose,
                        group = prename
                    ),
                    name  = "At least 1 dose") %>%
-            hc_add_series(df_vac3, "line",
+            hc_add_series(df_vac3,
+                          "line",
                           hcaes(
                               x = date,
                               y = proptotal_partially,
                               group = prename
                           ),
                           name  = "Partially vaccinated") %>%
-            hc_add_series(df_vac3, "line",
+            hc_add_series(df_vac3,
+                          "line",
                           hcaes(
                               x = date,
                               y = proptotal_fully,
                               group = prename
                           ),
                           name  = "Fully vaccinated") %>%
-            hc_add_series(df_vac3, "line",
-                          hcaes(
-                              x = date,
-                              y = proptotal_additional,
-                              group = prename),
-                          name  = "Fully vaccinated with an additional dose") %>%
+            hc_add_series(
+                df_vac3,
+                "line",
+                hcaes(
+                    x = date,
+                    y = proptotal_additional,
+                    group = prename
+                ),
+                name  = "Fully vaccinated with an additional dose"
+            ) %>%
             hc_yAxis(
-                title=list(text = "Percentage"),
-                tickInterval=25,
-                min=0,
-                max=100) %>%
-            hc_title(text = paste("Percentage of vaccined in", input$area_percentage))
+                title = list(text = "Percentage"),
+                tickInterval = 25,
+                min = 0,
+                max = 100
+            ) %>%
+            hc_title(
+                text = paste(
+                    "vaccinated Percentage of ",
+                    input$age_group,
+                    " in ",
+                    input$area_percentage
+                )
+            )
         return(plot)
     })
+    
+    
+    output$case_after_vac <-  renderHighchart({
+        plot <- vaccines2 %>%
+            hchart("pie", hcaes(x = "label.en", y = "prop_cases"),
+                   name = "Number of Cases") %>%
+            hc_title(text = "Cases")
+        
+        return(plot)
+    })
+    
+    output$case_after_vac2 <-  renderHighchart({
+        plot <- vaccines2 %>%
+            hchart("pie",
+                   hcaes(x = "label.en", y = "prop_hospitalizations"),
+                   name = "Number of Cases") %>%
+            hc_title(text = "Hospitalizations")
+        
+        return(plot)
+    })
+    
+    output$case_after_vac3 <-  renderHighchart({
+        plot <- vaccines2 %>%
+            hchart("pie", hcaes(x = "label.en", y = "prop_deaths"),
+                   name = "Number of Cases") %>%
+            hc_title(text = "Deaths")
+        
+        return(plot)
+    })
+    
+    
     
 })
--- ui.R
+++ ui.R
@@ -15,98 +15,116 @@ library(ggplot2)
 library(tidyverse)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-    # Application title
-    titlePanel("Covide-19 data dashboard"),
-    
-    fluidRow(highchartOutput("canadaMap")),
-    
-    fluidRow(
-        align = "center",
-        selectInput(
-            inputId = "total_record",
-            label = "Select the information you want to know.",
-            selected = "Daily Cases",
-            choices = c(
-                "Total Deaths",
-                "Total Tests",
-                "Total Cases",
-                "Total Partially vaccinated",
-                "Total Fully vaccinated",
-                "Total Additional vaccinated"
-            )
-        )
-    ),
-    
-    sidebarLayout(
-        sidebarPanel(
+shinyUI(
+    fluidPage(
+        # Application title
+        titlePanel("Covide-19 data dashboard"),
+        
+        fluidRow(highchartOutput("canadaMap")),
+        
+        fluidRow(
+            align = "center",
             selectInput(
-                inputId = "vac_dose",
-                label = "which does of vaccine you want to know?",
-                selected = "First Doses",
+                inputId = "total_record",
+                label = "Select the information you want to know.",
+                selected = "Daily Cases",
                 choices = c(
-                    "Partially vaccinated",
-                    "At least 1 dose vaccinated", 
-                    "Fully vaccinated",
-                    "Additional vaccinated"
+                    "Total Deaths",
+                    "Total Tests",
+                    "Total Cases",
+                    "Total Partially vaccinated",
+                    "Total Fully vaccinated",
+                    "Total Additional vaccinated"
+                )
+            )
+        ),
+        
+        sidebarLayout(
+            sidebarPanel(
+                selectInput(
+                    inputId = "vac_dose",
+                    label = "which does of vaccine you want to know?",
+                    selected = "First Doses",
+                    choices = c(
+                        "Partially vaccinated",
+                        "At least 1 dose vaccinated",
+                        "Fully vaccinated",
+                        "Additional vaccinated"
+                    )
+                ),
+                checkboxGroupInput(
+                    inputId = "area",
+                    label = "show outcome plot",
+                    selected = "Canada",
+                    choices = c(
+                        "Canada",
+                        "British Columbia",
+                        "Alberta",
+                        "Saskatchewan",
+                        "Manitoba",
+                        "Ontario",
+                        "Quebec",
+                        "New Brunswick",
+                        "Nova Scotia",
+                        "Newfoundland and Labrador",
+                        "Prince Edward Island",
+                        "Yukon",
+                        "Northwest Territories",
+                        "Nunavut"
+                    )
                 )
             ),
-            checkboxGroupInput(
-                inputId = "area",
-                label = "show outcome plot",
-                selected = "Canada",
-                choices = c(
-                    "Canada",
-                    "British Columbia",
-                    "Alberta",
-                    "Saskatchewan",
-                    "Manitoba",
-                    "Ontario",
-                    "Quebec",
-                    "New Brunswick",
-                    "Nova Scotia",
-                    "Newfoundland and Labrador",
-                    "Prince Edward Island",
-                    "Yukon",
-                    "Northwest Territories",
-                    "Nunavut"
-                )
-            )
+            
+            mainPanel(highchartOutput("time_series_vac"))
         ),
         
-        mainPanel(highchartOutput("time_series_vac"))
-    ),
-    
-    sidebarLayout(
-        sidebarPanel(
-            selectInput(
-                inputId = "area_percentage",
-                label = "show outcome plot",
-                selected = "Canada",
-                choices = c(
-                    "Canada",
-                    "British Columbia",
-                    "Alberta",
-                    "Saskatchewan",
-                    "Manitoba",
-                    "Ontario",
-                    "Quebec",
-                    "New Brunswick",
-                    "Nova Scotia",
-                    "Newfoundland and Labrador",
-                    "Prince Edward Island",
-                    "Yukon",
-                    "Northwest Territories",
-                    "Nunavut"
+        sidebarLayout(
+            sidebarPanel(
+                selectInput(
+                    inputId = "area_percentage",
+                    label = "Select province",
+                    selected = "Canada",
+                    choices = c(
+                        "Canada",
+                        "British Columbia",
+                        "Alberta",
+                        "Saskatchewan",
+                        "Manitoba",
+                        "Ontario",
+                        "Quebec",
+                        "New Brunswick",
+                        "Nova Scotia",
+                        "Newfoundland and Labrador",
+                        "Prince Edward Island",
+                        "Yukon",
+                        "Northwest Territories",
+                        "Nunavut"
+                    )
+                ),
+                selectInput(
+                    inputId = "age_group",
+                    label = "Select the by age",
+                    selected = "People",
+                    choices = c("People",
+                                "People 18 and older",
+                                "People 5 and older")
                 )
-            )
+            ),
+            
+            mainPanel(highchartOutput("vac_percentage"))
         ),
         
-        mainPanel(highchartOutput("vac_percentage"))
-    ),
-    
-    #fluidRow(plotlyOutput("vac_percentage"))
-    # fluidRow(highchartOutput("vac_percentage")),
-    
-    
-))
+        br(),
+        
+        fluidRow(
+            splitLayout(
+                cellWidths = c("33%", "33%", "33%"),
+                highchartOutput("case_after_vac"),
+                highchartOutput("case_after_vac2"),
+                highchartOutput("case_after_vac3")
+            )
+            
+        )
+        
+    )
+)
